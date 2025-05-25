bae12ea6690554a15fc7e7de277acc42f6efb894
Joaquim Azevedo <joaquimazeveddoo@gmail.com>
2025-05-24 22:47:41 -0300

    add put castrate animal and delete animal by id

--- src/main/java/com/petshop/petshop/controller/AnimalController.java
+++ src/main/java/com/petshop/petshop/controller/AnimalController.java
@@ -3,15 +3,18 @@ package com.petshop.petshop.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.petshop.petshop.dto.animal.AnimalDTO;
 import com.petshop.petshop.dto.animal.AnimalRequest;
+import com.petshop.petshop.dto.animal.AnimalResponse;
 import com.petshop.petshop.service.AnimalService;
 
 import jakarta.transaction.Transactional;
@@ -25,18 +28,30 @@ public class AnimalController {
     private AnimalService animalService;
 
     @GetMapping
-    public List<AnimalDTO> getAllAnimals() {
-        return animalService.getAllAnimals();
+    public List<AnimalResponse> getAllActiveAnimals() {
+        return animalService.getAllActiveAnimals();
     }
 
     @GetMapping("/{cpf}")
-    public List<AnimalDTO> getAnimalsByOwnerCpf(@PathVariable @Valid String cpf) {
+    public List<AnimalResponse> getAnimalsByOwnerCpf(@PathVariable @Valid String cpf) {
         return animalService.getAnimalsByOwnerCpf(cpf);
     }
 
     @Transactional
+    @PutMapping("/castrate/{id}")
+    public AnimalResponse castrateAnimalById(@PathVariable @Valid String id) {
+        return animalService.castrateAnimalById(id);
+    }
+
+    @Transactional
     @PostMapping
-    public AnimalRequest addAnimal(@RequestBody @Valid AnimalDTO dto) {
-        return animalService.addAnimal(dto);
+    public AnimalDTO addAnimal(@RequestBody @Valid AnimalRequest request) {
+        return animalService.addAnimal(request);
+    }
+
+    @Transactional
+    @DeleteMapping("{id}")
+    public void deleteAnimalById(@PathVariable @Valid String id) {
+        animalService.deleteAnimalById(id);
     }
 }
