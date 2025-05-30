a835c1eab7d84e37f8c02722952ef415ce01046a
Joaquim Azevedo <joaquimazeveddoo@gmail.com>
2025-05-30 14:35:51 -0300

    adding new breeds

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,8 @@
 INSERT INTO tb_animaltype_list (type) VALUES ('Cachorro');
 INSERT INTO tb_animaltype_list (type) VALUES ('Gato');
+INSERT INTO tb_animaltype_list (type) VALUES ('Pássaro');
+INSERT INTO tb_animaltype_list (type) VALUES ('Roedor');
+INSERT INTO tb_animaltype_list (type) VALUES ('Réptil');
 
 INSERT INTO tb_breeds (type_id, name, normalized_name) VALUES (1, 'Labrador Retriever', 'labradorretriever');
 INSERT INTO tb_breeds (type_id, name, normalized_name) VALUES (1, 'Poodle', 'poodle');
@@ -29,6 +32,7 @@ INSERT INTO tb_breeds (type_id, name, normalized_name) VALUES (1, 'Buldogue Fran
 INSERT INTO tb_breeds (type_id, name, normalized_name) VALUES (1, 'Setter Irlandês', 'setterirlandes');
 INSERT INTO tb_breeds (type_id, name, normalized_name) VALUES (1, 'Staffordshire Bull Terrier', 'staffordshirebullterrier');
 INSERT INTO tb_breeds (type_id, name, normalized_name) VALUES (1, 'Basset Hound', 'bassethound');
+
 INSERT INTO tb_breeds (type_id, name, normalized_name) VALUES (2, 'Siamês', 'siames');
 INSERT INTO tb_breeds (type_id, name, normalized_name) VALUES (2, 'Persa', 'persa');
 INSERT INTO tb_breeds (type_id, name, normalized_name) VALUES (2, 'Maine Coon', 'mainecoon');
@@ -49,6 +53,19 @@ INSERT INTO tb_breeds (type_id, name, normalized_name) VALUES (2, 'Cornish Rex',
 INSERT INTO tb_breeds (type_id, name, normalized_name) VALUES (2, 'Devon Rex', 'devonrex');
 INSERT INTO tb_breeds (type_id, name, normalized_name) VALUES (2, 'Selkirk Rex', 'selkirkrex');
 INSERT INTO tb_breeds (type_id, name, normalized_name) VALUES (2, 'Manx', 'manx');
+
+INSERT INTO tb_breeds (type_id, name, normalized_name) VALUES (3, 'Periquito', 'periquito');
+INSERT INTO tb_breeds (type_id, name, normalized_name) VALUES (3, 'Canário', 'canario');
+INSERT INTO tb_breeds (type_id, name, normalized_name) VALUES (3, 'Calopsita', 'calopsita');
+INSERT INTO tb_breeds (type_id, name, normalized_name) VALUES (3, 'Papagaio', 'papagaio');
+
+INSERT INTO tb_breeds (type_id, name, normalized_name) VALUES (4, 'Hamster Sírio', 'hamstersirio');
+INSERT INTO tb_breeds (type_id, name, normalized_name) VALUES (4, 'Porquinho-da-índia', 'porquinhodaindia');
+INSERT INTO tb_breeds (type_id, name, normalized_name) VALUES (4, 'Chinchila', 'chinchila');
+
+INSERT INTO tb_breeds (type_id, name, normalized_name) VALUES (5, 'Jabuti', 'jabuti');
+INSERT INTO tb_breeds (type_id, name, normalized_name) VALUES (5, 'Iguana Verde', 'iguanaverde');
+INSERT INTO tb_breeds (type_id, name, normalized_name) VALUES (5, 'Gecko-leopardo', 'geckoleopardo');
 
 INSERT INTO tb_owner (active, cpf, phone, email, name) VALUES (1, '00011122233', '00111112222', 'joaquim@teste.com', 'Joaquim Azevedo')
 INSERT INTO tb_animal (active, castrated, breed_id, owner_id, name, id) VALUES (1, 0, 9, 1, 'Sheik', '7ce715f9-534f-4f31-bf51-ce30f08459e8')
